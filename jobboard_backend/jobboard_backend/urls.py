287006a47129b04b405413ae958b1f9b69b2a630
Mesh-254 <meshack1995@gmail.com>
2025-03-10 09:51:41 +0300

    Implemented Host API documentation at /api/docs to facilitate frontend integration

--- jobboard_backend/jobboard_backend/urls.py
+++ jobboard_backend/jobboard_backend/urls.py
@@ -20,14 +20,15 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from realtimejobs.swagger import schema_view
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('swagger/', schema_view.with_ui('swagger',
-                          cache_timeout=0), name='schema-swagger-ui'),
+                                         cache_timeout=0), name='schema-swagger-ui'),
+    path('api/docs/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='redoc-docs'),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
     path('', include('realtimejobs.urls'))
-    
+
 ]
